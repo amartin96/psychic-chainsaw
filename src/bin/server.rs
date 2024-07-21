2de7fbd48eead6b51a07eae14612e46eac919eae
Alex Martin <alex.t.martin96@gmail.com>
2024-07-21 10:38:45 -0700

    server tracing otel stdout

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,5 @@
-use opentelemetry::trace::{Span, Tracer};
+use opentelemetry::trace::TracerProvider as _;
+use tracing_subscriber::layer::SubscriberExt as _;
 
 tonic::include_proto!("greeter");
 
@@ -12,12 +13,7 @@ impl greeter_server::Greeter for GreeterService {
         &self,
         request: tonic::Request<HelloRequest>,
     ) -> Result<tonic::Response<HelloReply>, tonic::Status> {
-        // tracing::info!("hello from server");
-
-        let tracer = opentelemetry::global::tracer("tracer");
-        let mut span = tracer.span_builder("span").start(&tracer);
-        span.add_event("event", vec![]);
-        span.end();
+        tracing::info!("hello from server");
 
         let reply = HelloReply {
             message: format!("Hello {}!", request.into_inner().name),
@@ -28,15 +24,19 @@ impl greeter_server::Greeter for GreeterService {
 
 #[tokio::main]
 async fn main() {
-    // let subscriber = tracing_subscriber::FmtSubscriber::new();
-    // tracing::subscriber::set_global_default(subscriber).unwrap();
+    let filter = tracing_subscriber::EnvFilter::try_new(format!(
+        "{}=trace",
+        env!("CARGO_BIN_NAME").replace("-", "_")
+    ))
+    .unwrap();
 
-    let exporter = opentelemetry_stdout::SpanExporter::default();
     let provider = opentelemetry_sdk::trace::TracerProvider::builder()
-        .with_simple_exporter(exporter)
-        // .with_config(opentelemetry_sdk::trace::Config::default())
+        .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
         .build();
-    opentelemetry::global::set_tracer_provider(provider);
+    let tracer = provider.tracer("my-tracer-name");
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = tracing_subscriber::Registry::default().with(filter).with(telemetry);
+    tracing::subscriber::set_global_default(subscriber).unwrap();
 
     let addr = "[::1]:50051".parse().unwrap();
     let greeter = GreeterService {};
